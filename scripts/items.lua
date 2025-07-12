43c06cbba090ab4c1d98801905aa400cae990ad8
Alex Bronner <acbronn@gmail.com>
2025-07-11 21:53:19 -0400

    Crystals are destroyed if tile underneath sinks.

--- scripts/items.lua
+++ scripts/items.lua
@@ -2,23 +2,32 @@ local mod = modApi:getCurrentMod()
 local path = mod.scriptPath
 local boardEvents = require(path .."libraries/boardEvents")
 
-local mine_damage = SpaceDamage(0)
-mine_damage.sAnimation = ""
-mine_damage.iCrack = EFFECT_REMOVE
+local RR_mine_damage = SpaceDamage(0)
+RR_mine_damage.sAnimation = ""
+RR_mine_damage.iCrack = EFFECT_REMOVE
 
 Item_RR_Crystal_Mine = { 
     Image = "combat/crystal_purp.png", 
-    Damage = mine_damage,
+    Damage = RR_mine_damage,
     Tooltip = "rr_crystal_mine",
     Icon = "combat/crystal_purp.png", 
     UsedImage = ""}
 
+--I do not like this approach to making vek not step on crystals
+local RR_OldScorePositioning = ScorePositioning
+function ScorePositioning(point, pawn)
+    local mission = GetCurrentMission()
+
+    if Board:GetItem(point) == "Item_RR_Crystal_Mine" then return -100 end
+
+    return RR_OldScorePositioning(point, pawn)
+end
+
+--If a mech consumes a crystal, give it boost
 boardEvents.onItemRemoved:subscribe(
     function(loc, removed_item)
         if removed_item == "Item_RR_Crystal_Mine"  then
-
             local pawn = Board:GetPawn(loc)
-
             if pawn then
                 local mine_damage = SpaceDamage(loc)
                 mine_damage.sScript = [[
@@ -26,6 +35,17 @@ boardEvents.onItemRemoved:subscribe(
                         Board:GetPawn(]]..loc:GetString()..[[):SetBoosted(true) 
                     end]]
                 Board:DamageSpace(mine_damage)
+            end
+        end
+    end)
+
+--If a crystal sinks, destroy it
+BoardEvents.onTerrainChanged:subscribe(
+    function(p, terrain, terrain_prev)
+        local item = Board:GetItem(p)
+        if item == "Item_RR_Crystal_Mine" then
+            if RR_IsSink_T(terrain) then
+                Board:RemoveItem(p)
             end
         end
     end)
--- scripts/terrain.lua
+++ scripts/terrain.lua
@@ -1,28 +1,33 @@
---If terrain is water/lava
+--If point's terrain is water/lava
 function RR_IsSink(point)
     local terrain = Board:GetTerrain(point)
+	return	RR_IsSink_T(terrain)
+end
+
+--If terrain is water/lava
+function RR_IsSink_T(terrain)
 	return	terrain == TERRAIN_WATER or terrain == TERRAIN_LAVA or terrain == TERRAIN_HOLE
 end
 
---If terrain is mountain
+--If point's terrain is mountain
 function RR_IsMountain(point)
 	return	Board:GetTerrain(point) == TERRAIN_MOUNTAIN
 end
 
---If terrain has rock on it
-function RR_HasRock(point)
-	local pawn = Board:GetPawn(point)
-	if pawn ~= nil then
-		return string.match(pawn:GetType(), "Wall")
+--If point's terrain has rock on it which will be destroyed by the given damage
+function RR_HasDeadRock(point, damage)
+	if Board:IsDeadly(damage, Pawn) then
+		return RR_HasRock(point)
 	else
 		return false
 	end
 end
 
---If terrain has rock on it which will be destroyed by the given damage
-function RR_HasDeadRock(point, damage)
-	if Board:IsDeadly(damage, Pawn) then
-		return RR_HasRock(point)
+--If point's terrain has rock on it
+function RR_HasRock(point)
+	local pawn = Board:GetPawn(point)
+	if pawn ~= nil then
+		return string.match(pawn:GetType(), "Wall")
 	else
 		return false
 	end
