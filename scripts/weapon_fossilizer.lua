c7f3b66b7f5b187c3e74a6d28bcaa50a4e5e24e6
Alex Bronner <acbronn@gmail.com>
2025-10-08 01:24:38 -0400

    Fossilizer upgrade preview!

--- scripts/weapon_fossilizer.lua
+++ scripts/weapon_fossilizer.lua
@@ -20,20 +20,46 @@ Pass_RR_Generic_Fossilizer = PassiveSkill:new{
 --Miner upgrade
 Pass_RR_Generic_Fossilizer_A = Pass_RR_Generic_Fossilizer:new{
     UpgradeDescription = "All rocks will spawn an energy crystal on death that gives mechs boost.",
-    Passive = "lmn_Passive_RockOnDeath_2"
+    Passive = "lmn_Passive_RockOnDeath_2",
+    TipImage = {
+        Unit = Point(2, 1),
+        Enemy = Point(1, 2),
+        Enemy2 = Point(2, 2),
+        Enemy3 = Point(3, 2),
+        CustomEnemy = "Wall"
+    }
 }
 
 --Skill Effect for mouseover preview
 function Pass_RR_Generic_Fossilizer:GetSkillEffect(p1, p2)
     local ret = SkillEffect()
-    for xPos = 1, 3 do                                              --Kill all 3 vek
+
+    --Kill all 3 vek
+    for xPos = 1, 3 do
         ret:AddDamage(SpaceDamage(Point(xPos, 2), DAMAGE_DEATH))
     end
-    ret:AddDelay(0.25)                                              --Wait a bit
-    for xPos = 1, 3 do                                              --Rocks!
-        local damage = SpaceDamage(Point(xPos, 2), 0)
-        damage.sPawn = "Wall"
+
+    --Wait a bit
+    ret:AddDelay(self.Passive == "lmn_Passive_RockOnDeath" and 0.25 or 1.50)
+
+    --Spawn rocks (main) or crystals (upgrade!)
+    for xPos = 1, 3 do
+        local point = Point(xPos, 2)
+        local damage = SpaceDamage(point, 0)
+
+        --Rocks or crystals!
+        if self.Passive == "lmn_Passive_RockOnDeath" then
+            damage.sPawn = "Wall"
+        else
+            damage.sScript = "Board:ClearSpace("..point:GetString()..")"
+            damage.sItem = "Item_RR_Crystal_Mine"
+        end
+
         ret:AddDamage(damage)
     end
+
+    --Wait a bit more
+    ret:AddDelay(self.Passive == "lmn_Passive_RockOnDeath" and 0.00 or 2.00)
+
     return ret
 end
